1b59e3261edf48725f66b2dd6635367dbdf5742d
Matthew Mattox <mmattox@support.tools>
2022-06-17 12:32:04 -0500

    WIP

---
+++ docker-build.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+printenv
+
+echo "Setting docker environment"
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD harbor.support.tools
+
+echo "Grabbing git commit hash"
+#GIT_COMMIT=$(git rev-list -1 HEAD)
+GIT_COMMIT=$DRONE_COMMIT_SHA
+echo "Git commit hash: $GIT_COMMIT"
+
+docker pull supporttools/k3s-janitor:latest
+echo "Building..."
+if ! docker build -t supporttools/k3s-janitor:${DRONE_BUILD_NUMBER} --cache-from supporttools/k3s-janitor:latest --build-arg REACT_APP_URL="/api" --build-arg GIT_COMMIT=${GIT_COMMIT} -f Dockerfile .
+then
+  echo "Docker build failed"
+  exit 127
+fi
+if ! docker push supporttools/k3s-janitor:${DRONE_BUILD_NUMBER}
+then
+  echo "Docker push failed"
+  exit 126
+fi
+echo "Tagging to latest and pushing..."
+if ! docker tag supporttools/k3s-janitor:${DRONE_BUILD_NUMBER} supporttools/k3s-janitor:latest
+then
+  echo "Docker tag failed"
+  exit 123
+fi
+if ! docker push supporttools/k3s-janitor:latest
+then
+  echo "Docker push failed"
+  exit 122
+fi
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/mattmattox/k3s-janitor
+
+go 1.17
+
+require (
+	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	golang.org/x/sys v0.0.0-20220615213510-4f61da869c0c // indirect
+)
