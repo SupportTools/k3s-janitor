488fd86f8d38e27ea2e21cb4627307cf43e16950
Matthew Mattox <mmattox@support.tools>
2022-06-21 09:47:41 -0500

    Cleaning up build script

--- docker-build.sh
+++ docker-build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-printenv
-
 echo "Setting docker environment"
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD harbor.support.tools
 
@@ -12,7 +10,7 @@ echo "Git commit hash: $GIT_COMMIT"
 
 docker pull supporttools/k3s-janitor:latest
 echo "Building..."
-if ! docker build -t supporttools/k3s-janitor:${DRONE_BUILD_NUMBER} --cache-from supporttools/k3s-janitor:latest --build-arg REACT_APP_URL="/api" --build-arg GIT_COMMIT=${GIT_COMMIT} -f Dockerfile .
+if ! docker build -t supporttools/k3s-janitor:${DRONE_BUILD_NUMBER} --cache-from supporttools/k3s-janitor:latest --build-arg GIT_COMMIT=${GIT_COMMIT} -f Dockerfile .
 then
   echo "Docker build failed"
   exit 127
