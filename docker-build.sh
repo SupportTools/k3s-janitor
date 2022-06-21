98ee61a82d6ad332ee0843544293b8b9ff4f2a65
Matthew Mattox <mmattox@support.tools>
2022-06-21 09:50:30 -0500

    Fixing creds

--- docker-build.sh
+++ docker-build.sh
@@ -3,14 +3,9 @@
 echo "Setting docker environment"
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD harbor.support.tools
 
-echo "Grabbing git commit hash"
-#GIT_COMMIT=$(git rev-list -1 HEAD)
-GIT_COMMIT=$DRONE_COMMIT_SHA
-echo "Git commit hash: $GIT_COMMIT"
-
 docker pull supporttools/k3s-janitor:latest
 echo "Building..."
-if ! docker build -t supporttools/k3s-janitor:${DRONE_BUILD_NUMBER} --cache-from supporttools/k3s-janitor:latest --build-arg GIT_COMMIT=${GIT_COMMIT} -f Dockerfile .
+if ! docker build -t supporttools/k3s-janitor:${DRONE_BUILD_NUMBER} --cache-from supporttools/k3s-janitor:latest -f Dockerfile .
 then
   echo "Docker build failed"
   exit 127
