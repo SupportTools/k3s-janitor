fb7fa1b373f11268dfb408048b9d22aadcc629ad
Matthew Mattox <mmattox@support.tools>
2022-06-23 21:17:11 -0500

    Adding multiarch and changing base running image

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 FROM golang:latest AS build_base
+ARG GIT_COMMIT
 WORKDIR /src
 COPY . .
 RUN go get -d -v ./...
 RUN go install -v ./...
-RUN export GIT_COMMIT=$(git rev-list -1 HEAD) && \
-CGO_ENABLED=0 go build -ldflags "-X main.GitCommit=$GIT_COMMIT" -o main main.go
+RUN CGO_ENABLED=0 go build -ldflags "-X main.GitCommit=$GIT_COMMIT" -o main main.go
 
-FROM scratch
+FROM ubuntu:latest
 COPY --from=build_base /src/main /main
 CMD ["/main"]
--- docker-build.sh
+++ docker-build.sh
@@ -1,28 +1,27 @@
 #!/bin/sh
 
-echo "Setting docker environment"
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+if [ -z "${DRONE_BUILD_NUMBER}" ]; then
+  echo "Building locally"
+  export LOCAL=true
+fi
 
-docker pull supporttools/k3s-janitor:latest
+if [[ ! ${LOCAL} == 'true' ]]; then
+  echo "Setting docker repo..."
+  docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+fi
+
+echo "Setting up build variables..."
+GIT_COMMIT=$(git rev-list -1 HEAD)
+
+echo "Setting up buildx..."
+docker buildx version
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+docker buildx create --name multiarch --use
+
 echo "Building..."
-if ! docker build -t supporttools/k3s-janitor:${DRONE_BUILD_NUMBER} --cache-from supporttools/k3s-janitor:latest -f Dockerfile .
+if [[ ${LOCAL} == 'true' ]]
 then
-  echo "Docker build failed"
-  exit 127
-fi
-if ! docker push supporttools/k3s-janitor:${DRONE_BUILD_NUMBER}
-then
-  echo "Docker push failed"
-  exit 126
-fi
-echo "Tagging to latest and pushing..."
-if ! docker tag supporttools/k3s-janitor:${DRONE_BUILD_NUMBER} supporttools/k3s-janitor:latest
-then
-  echo "Docker tag failed"
-  exit 123
-fi
-if ! docker push supporttools/k3s-janitor:latest
-then
-  echo "Docker push failed"
-  exit 122
+  docker build build --platform linux/amd64,linux/arm64 --build-arg GIT_COMMIT=${GIT_COMMIT} -f Dockerfile .
+else
+  docker build build --platform linux/amd64,linux/arm64 -t supporttools/k3s-janitor:${DRONE_BUILD_NUMBER} -t supporttools/k3s-janitor:latest --cache-from supporttools/k3s-janitor:latest --build-arg GIT_COMMIT=${GIT_COMMIT} --push -f Dockerfile .
 fi
