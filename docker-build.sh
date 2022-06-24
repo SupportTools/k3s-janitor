cf487899f8b55308c313e95e9c3cbb4980b121df
Matthew Mattox <mmattox@support.tools>
2022-06-23 21:21:33 -0500

    Removing local builds

--- docker-build.sh
+++ docker-build.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
-if [ -z "${DRONE_BUILD_NUMBER}" ]; then
-  echo "Building locally"
-  export LOCAL=true
-fi
-
-if [[ ! ${LOCAL} == 'true' ]]; then
-  echo "Setting docker repo..."
-  docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-fi
+echo "Setting docker repo..."
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
 echo "Setting up build variables..."
 GIT_COMMIT=$(git rev-list -1 HEAD)
@@ -19,9 +12,4 @@ docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx create --name multiarch --use
 
 echo "Building..."
-if [[ ${LOCAL} == 'true' ]]
-then
-  docker buildx build --platform linux/amd64,linux/arm64 --build-arg GIT_COMMIT=${GIT_COMMIT} -f Dockerfile .
-else
-  docker buildx build --platform linux/amd64,linux/arm64 -t supporttools/k3s-janitor:${DRONE_BUILD_NUMBER} -t supporttools/k3s-janitor:latest --cache-from supporttools/k3s-janitor:latest --build-arg GIT_COMMIT=${GIT_COMMIT} --push -f Dockerfile .
-fi
+docker buildx build --platform linux/amd64,linux/arm64 -t supporttools/k3s-janitor:${DRONE_BUILD_NUMBER} -t supporttools/k3s-janitor:latest --cache-from supporttools/k3s-janitor:latest --build-arg GIT_COMMIT=${GIT_COMMIT} --push -f Dockerfile .
