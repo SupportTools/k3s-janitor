f09c7c5471c84b20ecb90663f20918029a967c12
Matthew Mattox <mmattox@support.tools>
2022-06-27 02:19:09 -0500

    Testing command as sh

--- main.go
+++ main.go
@@ -136,7 +136,7 @@ func main() {
 	}
 
 	// Verify that crictl is available
-	cmd := exec.Command("crictl", "--version")
+	cmd := exec.Command("/bin/sh", "-c", "crictl --version")
 	stdout, err := cmd.Output()
 	if err != nil {
 		log.Println("crictl not available")
@@ -146,7 +146,7 @@ func main() {
 	log.Print(string(stdout))
 
 	// Dump crictl info
-	cmd = exec.Command("crictl", "info")
+	cmd = exec.Command("/bin/sh", "-c", "crictl info")
 	stdout, err = cmd.Output()
 	if err != nil {
 		log.Println("crictl info not available")
